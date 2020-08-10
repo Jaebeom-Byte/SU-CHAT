c8d9243b925c4bd44d0e41931ec74bc920f2a656
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-08-10 17:36:16 +0900

    test commit*APIController

--- src/main/java/kr/ac/syu/controller/ChatAPIController.java
+++ src/main/java/kr/ac/syu/controller/ChatAPIController.java
@@ -1,31 +1,17 @@
 package kr.ac.syu.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ChatAPIController {
-	@RequestMapping(value="/ChatAPIController.chat", method=RequestMethod.GET)
-	public void messages(@RequestParam(value="msg", required=false) String msg,
-						 HttpServletResponse response) {
-		System.out.println(msg);
-		PrintWriter out = null;
-		try {
-			out = response.getWriter();
-			out.print(msg);
-			out.flush();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			if(out != null) out.close();
-		}
+	@ResponseBody
+	@RequestMapping(value="/ChatAPIController.chat", method=RequestMethod.POST)
+	public String message(@RequestBody String message) {
+		System.out.println(message);
+		return message;
 	}
 }
