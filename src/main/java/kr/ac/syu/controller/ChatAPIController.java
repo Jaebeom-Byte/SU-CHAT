ef0bdf1b5ff8a063d6723e7c8e95b0241e5225d9
AsterGoldman <kyumin3537@naver.com>
2020-08-12 17:31:58 +0900

    형태소 분석이 콘솔에 찍힘

--- src/main/java/kr/ac/syu/controller/ChatAPIController.java
+++ src/main/java/kr/ac/syu/controller/ChatAPIController.java
@@ -1,12 +1,7 @@
 package kr.ac.syu.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.ac.syu.service.ChatService;
 import net.sf.json.JSONObject;
 
 @Controller
@@ -33,43 +29,39 @@ public class ChatAPIController {
 //		String url = "http://127.0.0.1:5110/syu/v1/";
 		
 //		jsonObj.put("message", getRequestToPyhtonServer(url, msg));
+		
+		ChatService csv= new ChatService();
+		csv.Komoran(msg);
+		
+		
 		jsonObj.put("message", msg);
 		jsonResponseToClient(response, jsonObj);
 	}
 	
-	@SuppressWarnings("unused")
-	private String getRequestToPyhtonServer(String gURL, String message) {
-		String result = "";
-		
-		try {
-			message = URLEncoder.encode(message, "UTF-8");
-			
-			URL url = new URL(gURL + message);
-			HttpURLConnection http = (HttpURLConnection)url.openConnection();
-			
-			http.setRequestMethod("GET");
-			http.setDoInput(true);
-			http.setDoOutput(false);
-			
-			InputStreamReader isr
-				= new InputStreamReader(http.getInputStream(), "UTF-8");
-			BufferedReader br = new BufferedReader(isr);
-			StringBuilder sb = new StringBuilder();
-			String str = "";
-			while((str = br.readLine()) != null) {
-				sb.append(str + "\n");
-			}
-			result = sb.toString();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
+	/*
+	 * @SuppressWarnings("unused") private String getRequestToPyhtonServer(String
+	 * gURL, String message) { String result = "";
+	 * 
+	 * try { message = URLEncoder.encode(message, "UTF-8");
+	 * 
+	 * URL url = new URL(gURL + message); HttpURLConnection http =
+	 * (HttpURLConnection)url.openConnection();
+	 * 
+	 * http.setRequestMethod("GET"); http.setDoInput(true); http.setDoOutput(false);
+	 * 
+	 * InputStreamReader isr = new InputStreamReader(http.getInputStream(),
+	 * "UTF-8"); BufferedReader br = new BufferedReader(isr); StringBuilder sb = new
+	 * StringBuilder(); String str = ""; while((str = br.readLine()) != null) {
+	 * sb.append(str + "\n"); } result = sb.toString(); } catch (Exception e) {
+	 * e.printStackTrace(); } return result; }
+	 */
+	
 	
 	private void jsonResponseToClient(HttpServletResponse response, JSONObject jsonObj) {
 		PrintWriter out = null;
 		try {
 			out = response.getWriter();
+			
 			out.print(jsonObj);
 			out.flush();
 		} catch (IOException e) {
--- src/main/java/kr/ac/syu/service/ChatService.java
+++ src/main/java/kr/ac/syu/service/ChatService.java
@@ -1,5 +1,47 @@
 package kr.ac.syu.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
+import kr.co.shineware.nlp.komoran.model.Token;
+
+@Service
 public class ChatService {
+	/*
+	 * Komoran komoran = new Komoran(DEFAULT_MODEL.FULL); 
+	 * String strToAnalyze =
+	 * "대한민국은 민주공화국이다.";
+	 * 
+	 * KomoranResult analyzeResultList = komoran.analyze(strToAnalyze);
+	 * 
+	 * 
+	 * 
+	 * List<Token> tokenList = analyzeResultList.getTokenList(); 
+	 * for (Token token :tokenList) { System.out.format("(%2d, %2d) %s/%s\n", token.getBeginIndex(),
+	 * token.getEndIndex(), token.getMorph(), token.getPos()); }
+	 */
+	public List<Token> Komoran(String message) {
+		Komoran komoran =new Komoran(DEFAULT_MODEL.FULL);
+		
+		String strToAnalyze=message;
+		KomoranResult analyzeResultList = komoran.analyze(strToAnalyze);
+		
+				
+		List<Token> tokenList = analyzeResultList.getTokenList();
+		for(Token token : tokenList) {
+			System.out.format("(%2d, %2d) %s/%s\n", token.getBeginIndex(),
+			 token.getEndIndex(), token.getMorph(), token.getPos());
+			
+		}
+		
+		return tokenList;
+	}
+	
 	
 }
+
+
