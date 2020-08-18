c978420ffbf57bea901cc46aa43215ee3b98ebb9
dayoonKim <dayoonkim138@gmail.com>
2020-08-18 18:13:20 +0900

    유사어>>표준어 정정 완료 및 도착지 키워드 추출 완료

---
+++ src/main/java/kr/ac/syu/core/DestinationExtractor.java
@@ -0,0 +1,50 @@
+package kr.ac.syu.core;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DestinationExtractor {
+	public static DestinationExtractor destinationExtractor;
+	private Generator gen;
+	private List<String> trimmedNouns;
+
+	private DestinationExtractor() {
+		gen = Generator.getGenerator();
+		trimmedNouns = new ArrayList<String>();
+	}
+	
+	public DestinationExtractor getDestinationExtractor() {
+		if(destinationExtractor == null) {
+			destinationExtractor = new DestinationExtractor();
+		}
+		return destinationExtractor;
+	}
+	
+	public String printTrimmedNouns(List<String> refinedNouns) throws Exception {
+		mapRefinedOntoTrimmed(refinedNouns);
+		String destinationKeyword = trimmedNouns.toString().replace(" ", "");
+
+		return destinationKeyword;
+	}
+
+	private void mapRefinedOntoTrimmed(List<String> refinedNouns) throws Exception {
+		List<String> dictionary = gen.genarateDictionary("/resources/dictionary.properties");
+		HashMap<String, String> thesaurus = gen.genarateThesaurusMap("/resources/thesaurus.properties");
+		
+		refinedToTrimmed: for (String refinedNoun : refinedNouns) {
+			for (String standardWord : dictionary) {
+				if (refinedNoun.equals(standardWord)) {
+					trimmedNouns.add(standardWord);
+				} else {
+					for (String synonym : thesaurus.keySet()) {
+						if (refinedNoun.equals(synonym)) {
+							trimmedNouns.add(thesaurus.get(synonym));
+							continue refinedToTrimmed;
+						}
+					}
+				}
+			}
+		}
+	}
+}
