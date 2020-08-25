9c22c412f76df11f0936e20afe3da936e0961e5c
didwn <didwn@DESKTOP-MQONDR6>
2020-08-25 18:35:06 +0900

    url센더

--- src/main/java/kr/ac/syu/core/crawler/URLSender.java
+++ src/main/java/kr/ac/syu/core/crawler/URLSender.java
@@ -1,5 +1,6 @@
 package kr.ac.syu.core.crawler;
 
+import kr.ac.syu.core.crawler.AbstractCrawler;
 
 public abstract class URLSender extends AbstractCrawler {
 	protected String linkPass(String url) {
