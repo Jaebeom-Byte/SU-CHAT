16da05a807967320690482e24bb37e3d76aa34ab
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-08-26 11:37:48 +0900

    크롤러 FIX

--- src/main/java/kr/ac/syu/core/crawler/AbstractCrawler.java
+++ src/main/java/kr/ac/syu/core/crawler/AbstractCrawler.java
@@ -17,14 +17,17 @@ public abstract class AbstractCrawler implements Crawler {
 	AbstractCrawler() {
 		try {
 			System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
-			driver = new ChromeDriver();
 			options = new ChromeOptions();
-			options.addArguments("start-maximized"); // 최대크기로
+			options.setCapability("ignoreProtectedModeSettings", true);
 			options.addArguments("headless"); // Browser를 띄우지 않음
 			options.addArguments("disable-gpu"); // GPU를 사용하지 않음, Linux에서 headless를 사용하는 경우 필요함.
-			options.addArguments("no-sandbox"); // Sandbox 프로세스를 사용하지 않음, Linux에서 headless를 사용하는 경우 필요함.		
+			options.addArguments("no-sandbox"); // Sandbox 프로세스를 사용하지 않음, Linux에서 headless를 사용하는 경우 필요함.
+			options.addArguments("disable-dev-shm-usage");
+			options.addArguments("lang=ko");
+			driver = new ChromeDriver(options);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 }
+
