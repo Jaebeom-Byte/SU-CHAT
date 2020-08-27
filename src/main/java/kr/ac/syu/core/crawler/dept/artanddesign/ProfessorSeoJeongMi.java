5277a09e5b78b72bb98fc54c7fc4e5a60ee6f11d
didwn <didwn@DESKTOP-MQONDR6>
2020-08-27 19:26:25 +0900

    아트앤디자인교수님 수정본입니다 dept들어간 경로입니다

--- src/main/java/kr/ac/syu/core/crawler/dept/artanddesign/ProfessorSeoJeongMi.java
+++ src/main/java/kr/ac/syu/core/crawler/dept/artanddesign/ProfessorSeoJeongMi.java
@@ -5,6 +5,6 @@ import kr.ac.syu.core.crawler.CaptureCrawler;
 public class ProfessorSeoJeongMi extends CaptureCrawler {
 	@Override
 	public String process() {
-		return crawImage("https://www.syu.ac.kr/cse/faculty/professor/", "//*[@id=\"post-54\"]/div[2]/ul/li[5]/div/div[1]");
+		return crawImage("https://www.syu.ac.kr/arts/faculty/professor/", "//*[@id=\"post-54\"]/div[2]/ul/li[5]/div/div[1]");
 	}
 }
