5b663d66cb520b4a3b27ffda1f83e0bf72337488
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-08-29 00:21:06 +0900

    InitCrawler() 메소드 접근제한자 private -> protected

--- src/main/java/kr/ac/syu/core/crawler/AbstractCrawler.java
+++ src/main/java/kr/ac/syu/core/crawler/AbstractCrawler.java
@@ -22,12 +22,12 @@ public abstract class AbstractCrawler implements Crawler {
 		}
 	}
 	
-	private void initCrawler() throws Exception {
+	protected void initCrawler() throws Exception {
 		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
 		options = new ChromeOptions();
 		options.setCapability("ignoreProtectedModeSettings", true);
 		options.addArguments("headless"); // Browser를 띄우지 않음
-		options.addArguments("window-size=1920x1080");
+		options.addArguments("window-size=3840x2160");
 		options.addArguments("disable-gpu"); // GPU를 사용하지 않음, Linux에서 headless를 사용하는 경우 필요함.
 		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36");
 		options.addArguments("no-sandbox"); // Sandbox 프로세스를 사용하지 않음, Linux에서 headless를 사용하는 경우 필요함.
