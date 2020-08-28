1d203fd35db66ebd3fa5f02f55288ec14d4f1461
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-08-28 22:38:08 +0900

    필요없는 클래스 정리

--- src/main/java/kr/ac/syu/controller/ChatAPIController.java
+++ src/main/java/kr/ac/syu/controller/ChatAPIController.java
@@ -3,10 +3,8 @@ package kr.ac.syu.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/kr/ac/syu/repository/ChatDao.java
+++
@@ -1,5 +0,0 @@
-package kr.ac.syu.repository;
-
-public class ChatDao {
-
-}
