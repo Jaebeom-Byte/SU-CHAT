d0f366d9f2356afc79ddfc7b9f3a479326c5ac17
dayoonKim <dayoonkim138@gmail.com>
2020-08-28 15:17:11 +0900

    테스트 113-224 에러 개선(각종 .properties, 클래스명 변경)

--- src/main/java/kr/ac/syu/crawler/selfgoverningbody/StudentCouncil.java
+++ src/main/java/kr/ac/syu/crawler/selfgoverningbody/StudentCouncil.java
@@ -2,11 +2,11 @@ package kr.ac.syu.crawler.selfgoverningbody;
 
 import kr.ac.syu.core.crawler.CaptureCrawler;
 
-public class StudentCouncil extends CaptureCrawler{ //총학생회
+public class Society extends CaptureCrawler{ //학회
 	@Override
 	public String process() {
 		return crawImage("https://www.syu.ac.kr/school-life/student-autonomy-organization/student-autonomy-organization-all/",
-				"//*[@id=\"post-2597\"]/div/div/ul/li[1]/div");
+				"//*[@id=\"post-2597\"]/div/div/ul/li[3]/div");
 		
 	}
 
--- src/main/java/kr/ac/syu/crawler/selfgoverningbody/StudentGovernment.java
+++ src/main/java/kr/ac/syu/crawler/selfgoverningbody/StudentGovernment.java
@@ -2,11 +2,11 @@ package kr.ac.syu.crawler.selfgoverningbody;
 
 import kr.ac.syu.core.crawler.CaptureCrawler;
 
-public class Society extends CaptureCrawler{ //학회
+public class StudentCouncil extends CaptureCrawler{ //총학생회
 	@Override
 	public String process() {
 		return crawImage("https://www.syu.ac.kr/school-life/student-autonomy-organization/student-autonomy-organization-all/",
-				"//*[@id=\"post-2597\"]/div/div/ul/li[3]/div");
+				"//*[@id=\"post-2597\"]/div/div/ul/li[1]/div");
 		
 	}
 
